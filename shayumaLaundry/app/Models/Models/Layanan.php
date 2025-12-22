9b709ff06e21673d11cd4a59e578aa25f2e810fe
satyaprabaswara <135005706+satyaprabaswara@users.noreply.github.com>
2025-12-22 21:23:32 +0700

    sattt

---
+++ shayumaLaundry/app/Models/Models/Layanan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Layanan extends Model
+{
+    //
+}
--- shayumaLaundry/routes/web.php
+++ shayumaLaundry/routes/web.php
@@ -1,11 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('laundry.index');
+});
 
 Route::get('/laundry', function () {
     return view('laundry.index');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/order', function () {
+        return view('order');
+    });
 });
